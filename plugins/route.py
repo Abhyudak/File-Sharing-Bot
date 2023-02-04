82c3a9c64e6504002a11e28a3846d45692fc39e7
Rᴏᴢᴀʀɪᴀɴ..!!! <105523705+Abhyudak@users.noreply.github.com>
2023-02-05 00:44:30 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -2,9 +2,6 @@
 #rymme
 
 
-
-
-
 from aiohttp import web
 
 routes = web.RouteTableDef()
@@ -12,3 +9,9 @@ routes = web.RouteTableDef()
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
     return web.json_response("CodeXBotz")
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
+
